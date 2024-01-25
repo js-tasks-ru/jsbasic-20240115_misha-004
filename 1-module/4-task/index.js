594ec93445f7bde002357140e18bb710a46de13a
misha004 <mishakotolup@yandex.ru>
2024-01-25 09:27:28 +0300

    Решение 4-ой задачи

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,7 @@
 function checkSpam(str) {
-  // ваш код...
+  str = str.toLowerCase();
+
+  return str.includes('1xbet') || str.includes('xxx');
 }
+
+// console.log( checkSpam('innocent rabbit') );
