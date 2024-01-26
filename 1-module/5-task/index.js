ad6632a94ef7d0bac1367ccfa603004df98daaac
misha004 <mishakotolup@yandex.ru>
2024-01-26 21:37:49 +0300

    Решение 5-ой задачи

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,17 @@
 function truncate(str, maxlength) {
-  // ваш код...
+  if(str.length > maxlength) {
+   
+    return str.slice(0, maxlength-1) + "…";
+  
+  } else {
+    
+    return str;
+  }
 }
+
+truncate('Вот, что мне хотелось бы сказать на эту тему:', 20)
+
+
+
+
+
