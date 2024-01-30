693603cd8dddebc1e3f2734f1237f906324f599d
misha004 <mishakotolup@yandex.ru>
2024-01-30 15:53:35 +0300

    Модуль 2. Решение 1-ой задачи

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,26 @@
-function sumSalary(salaries) {
-  // ваш код...
+let salaries = {
+  John: 1000,
+  Ann: 1600,
+  Pete: 1300,
+  month: 'December',
+  currency: 'USD',
+  isPayed: false
 }
+
+
+function sumSalary(salaries) {
+ 
+  let result = 0;
+
+  for(let key in salaries) {
+
+    if (typeof salaries[key] == 'number' && isFinite(salaries[key]) ) {
+      result += salaries[key]
+
+    }
+      
+    }
+    return result
+  }
+
+  
