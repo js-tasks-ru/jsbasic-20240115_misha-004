8180e53d761712b54debbae905d8ef3703533a2c
misha004 <mishakotolup@yandex.ru>
2024-01-31 21:08:33 +0300

    Модуль 2. Решение 3-ей задачи

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,25 @@
 let calculator = {
-  // ваш код
+
+  sum() {
+    return this.a + this.b;
+  },
+
+  mul() {
+    return this.a * this.b;
+  },
+
+  read(a, b) {
+    this.a = a;
+    this.b = b;
+  }
+ 
 };
+
+calculator.read(3, 5);
+calculator.sum();
+calculator.mul();
+
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
+// \
