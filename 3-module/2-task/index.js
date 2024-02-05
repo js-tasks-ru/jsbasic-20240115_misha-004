54611a712d7b66e6776fe9f764bf348041a8c53f
misha004 <mishakotolup@yandex.ru>
2024-02-05 23:52:46 +0300

    3 модуль. Решение задач

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,3 +1,5 @@
 function filterRange(arr, a, b) {
-  // ваш код...
+   return arr.filter( function(item) {
+      return a <= item && b >= item
+   } )
 }
--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,23 @@
-function camelize(str) {
-  // ваш код...
+function camelize(str) { 
+  let strSplited = str.split('-', )
+    
+  
+  if(strSplited[0] !== '' ) {
+     
+    let maped = strSplited.map( item => (item[0].toUpperCase() + item.slice(1) ) )
+  
+  
+    let joined = maped.join('')
+  
+    return joined[0].toLowerCase() + joined.slice(1)
+      
+  } else {
+      strSplited.splice(0, 1)
+
+      maped = strSplited.map( item => (item[0].toUpperCase() + item.slice(1) ) )
+
+      joined = maped.join('')
+    
+      return joined
+  }
 }
